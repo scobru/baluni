46658900dfdd5aec85fa01cfb067624d52017d03
scobru <scobru1988@gmail.com>
2023-12-26 23:37:32 +0100

    use 1inch spot price aggregator

--- src/pumpOrDump.ts
+++ src/pumpOrDump.ts
@@ -1,14 +1,17 @@
-import { pumpOrDump } from './pancakeswap/interact'
-import { promisify } from 'util'
+import { promisify } from "util";
+import { swap } from "./uniswap/swap";
+import { initializeWallet } from "./dexWallet";
+import { POLYGON } from "./networks";
 
-const token1 = process.argv[2]
-const token2 = process.argv[3]
-const action = process.argv[4]
+const token1 = process.argv[2];
+const token2 = process.argv[3];
+const action = process.argv[4];
 
-const main = promisify(async () => {
-    await pumpOrDump(token1, token2, action)
-  })
+const main = async () => {
+  const dexWallet = await initializeWallet(POLYGON[0]);
+  await swap(dexWallet, [token1, token2], action == "pump" ? false : true);
+};
 
 main().then(() => {
-    console.log('Async operation completed')
-})
+  console.log("Async operation completed");
+});
