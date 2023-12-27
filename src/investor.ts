ae278b87daf9a373dd9b299a453833b81cbd34b7
scobru <scobru1988@gmail.com>
2023-12-27 11:33:26 +0100

    add env.example

--- src/investor.ts
+++ src/investor.ts
@@ -3,14 +3,13 @@ import { TOKENS, WEIGHTS_UP, USDC } from "./config";
 import { invest } from "./uniswap/invest";
 import { POLYGON } from "./networks";
 
-const sellAll = String(process.argv[2]) === "true" ? "true" : "false";
+const sellAll = Boolean(process.argv[2]);
 
 async function investor() {
   try {
     // Initialize your DexWallet here
     const dexWallet = await initializeWallet(POLYGON[0]);
-
-    await invest(dexWallet, WEIGHTS_UP, USDC, TOKENS, Boolean(sellAll));
+    await invest(dexWallet, WEIGHTS_UP, USDC, TOKENS, sellAll);
   } catch (error) {
     console.error("Error during initialization:", error);
   }
