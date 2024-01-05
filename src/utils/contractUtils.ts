609d86b5c4b305c25411a7e8afb3876f0b2b329c
scobru <scobru1988@gmail.com>
2024-01-05 11:35:06 +0100

    folder reorganize

--- src/utils/contractUtils.ts
+++ src/utils/contractUtils.ts
@@ -1,6 +1,6 @@
 import { BigNumber, Contract, providers } from "ethers";
 import fs from "fs";
-import { POLYGON } from "./utils/networks";
+import { POLYGON } from "./networks";
 import { DexWallet } from "./dexWallet";
 import { loadPrettyConsole } from "./prettyConsole";
 
