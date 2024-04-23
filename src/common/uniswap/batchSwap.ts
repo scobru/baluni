ee77e3d8da1691f774b03a125a603b8603c475f5
scobru <scobru1988@gmail.com>
2024-04-23 23:54:46 +0200

    fixes

--- src/common/uniswap/batchSwap.ts
+++ src/common/uniswap/batchSwap.ts
@@ -3,9 +3,9 @@ import { ethers } from 'ethers'
 import { DexWallet } from '../../utils/web3/dexWallet'
 import { waitForTx } from '../../utils/web3/networkUtils'
 import { NETWORKS, INFRA, BASEURL } from 'baluni-api'
-//import { buildSwapUniswap } from 'baluni-api'
+import { buildSwapUniswap } from 'baluni-api'
 
-import { buildSwapUniswap } from '../../../../baluni-api/dist/uniswap/actions/buildSwapUniswap'
+//import { buildSwapUniswap } from '../../../../baluni-api/dist/uniswap/actions/buildSwapUniswap'
 
 export async function batchSwap(
   swaps: Array<{
