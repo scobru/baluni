6cf1e2ba8521676f3cb49c96228e494a2ed4c158
scobru <scobru1988@gmail.com>
2024-04-29 21:49:49 +0200

    v2.2.0

--- src/utils/quote.ts
+++ src/utils/quote.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers'
 import uniswapV3FactoryAbi from 'baluni-api/dist/abis/uniswap/Factory.json'
-import uniswapV3PoolAbi from 'baluni-api/dist/abis/uniswap/UniswapV3Pool.json'
+import uniswapV3PoolAbi from 'baluni-api/dist/abis/uniswap/Pool.json'
 import erc20Abi from 'baluni-api/dist/abis/common/ERC20.json' // Assuming you have ERC20 ABI for fetching decimals
 import { PROTOCOLS } from 'baluni-api'
 
