94e7d1e43e407c0f40fed903942823af4bb47cc6
scobru <scobru1988@gmail.com>
2024-05-19 01:18:33 +0200

    transfer ui folder into baluni-ui repo

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,9 +1,3 @@
-export {
-  rebalancePortfolio,
-  rebalancePortfolioOdos,
-  rebalancePortfolioOdosParams,
-  calculateRebalanceStats,
-} from './ui/rebalanceSimple'
 export { predict } from './features/ml/predict'
 export { executeRebalance } from './strategies/rebalance/rebalance'
 export { executeRebalanceV2 } from './strategies/rebalanceV2/rebalance'
